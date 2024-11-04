e47cd215340ec3ae4e79776adcfb06f932fc0b67
Joe Keohan <jkeohan@gmail.com>
2024-11-04 14:15:12 -0500

    build: initial build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import * as SDK from 'dc-extensions-sdk';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	useEffect(() => {
+		const initialize = async () => {
+
+			const sdk = await SDK.init();
+      console.log(SDK);
+		};
+		initialize();
+	}, []);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+					Extensions Testing
+			</header>
+		</div>
+	);
 }
 
 export default App;
